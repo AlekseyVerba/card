3c1b0078f7fcf02397ed318dfa3ffffc78e48824
AlekseyVerba <leha.verba.01@mail.ru>
2020-12-22 01:34:38 +0800

    card

--- js/main.js
+++ js/main.js
@@ -1,20 +1,42 @@
-const burger = $(".menu-link");
-const clos = $(".close-btn");
-const link = $("li a");
-const menu = $("menu");
 
 
-burger.click(function() {
-    menu.toggleClass("active-menu");
+
+
+$(".input-cart-number").on("keyup change", function() {
+    if($(this).val().length >= 4) {
+        $(this).next().focus();
+    }
+    if ($(this).val().length < 1) {
+        $(this).prev().focus();
+    }
+
+    $(this).val($(this).val().replace(/[A-Za-zА-Яа-яЁё]/, ''))
+
+    let index = $(this).index();
+    $(`.number span:nth-child(${index})`).text($(this).val());
+
+    if ($(this).index() === 4 && $(this).val().length >= 4) {
+        $("#card-holder").focus();
+    }
 });
 
-clos.click(function() {
-    menu.toggleClass("active-menu");
+$("#card-holder").on("keyup change", function() {
+    $(".card-holder div").text($(this).val());
 });
 
-link.on("click", function(e) {
-    e.preventDefault();
-    const id = $(this).attr("href");
-    const top = $(id).offset().top;
-    $("html, body").animate({scrollTop: top}, 1000);
+
+$("#card-expiration-month").on("change", function() {
+    $(".month").text($(this).val() + "/");
+});
+
+$("#card-expiration-year").on("change", function() {
+    $(".year").text($(this).val());
+});
+
+$("#card-ccv").on("focus", function() {
+    $(".credit-card-box").addClass("hover");
+}).on("blur", function() {
+    $(".credit-card-box").removeClass("hover");
+}).on("change keyup", function() {
+    $(".ccv div").text($(this).val());
 });
