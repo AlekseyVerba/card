fa43529836ca60afbe71b9a7d885f8399ec5d7d5
AlekseyVerba <leha.verba.01@mail.ru>
2020-12-21 21:33:45 +0800

    menu

--- js/main.js
+++ js/main.js
@@ -1,59 +1,20 @@
-$(document).ready(function () {
-    let count = 1;
-    let countSlide = $(".slide").length;
-    for(let i = 0; i < countSlide; i++) {
-        const dot = document.createElement("div");
-        dot.classList.add("dot");
-        $(".nav").append(dot);
-        if (i === 0) {
-            dot.classList.add("active");
-        }
-    }
+const burger = $(".menu-link");
+const clos = $(".close-btn");
+const link = $("li a");
+const menu = $("menu");
 
-    function changeDots(i) {
-        $(".dot").removeClass("active");
-        $(`.dot:eq(${i - 1})`).addClass("active");
-    }
 
-    $(".nav").on("click", ".dot", function() {
-        $(".dot").removeClass("active");
-        $(this).addClass("active");
-        let indexBtn = $(this).index();
-        if (indexBtn + 1 != count) {
-            const chet = -$(".view").height() * indexBtn;
-            $(".sliderWrapper").css(`transform`, `translate(0, ${chet}px)`);
-            count = indexBtn + 1; 
-        }
-    });
-    
-    function next() {
-        if (count >= countSlide) {
-            $(".sliderWrapper").css("transform", "translate(0, 0)");
-            count = 1;
-            changeDots(count);
-        } else {
-            console.log("else");
-            const chet = -$(".view").height() * count;
-            $(".sliderWrapper").css(`transform`, `translate(0, ${chet}px)`);
-            count++; 
-            changeDots(count);
-        }
-    }
+burger.click(function() {
+    menu.toggleClass("active-menu");
+});
 
-    function prev() {
-        if (count <= 1) {
-            let chet = -$(".view").height() * (countSlide - 1);
-            $(".sliderWrapper").css("transform", `translate(0, ${chet}px)`);
-            count = countSlide;
-            changeDots(count);
-        } else {
-            const chet = -$(".view").height() * (count - 2);
-            $(".sliderWrapper").css(`transform`, `translate(0, ${chet}px)`);
-            count--; 
-            changeDots(count);
-        }
-    }
-    
-    $(".right").on("click", next);
-    $(".left").on("click", prev);
+clos.click(function() {
+    menu.toggleClass("active-menu");
+});
+
+link.on("click", function(e) {
+    e.preventDefault();
+    const id = $(this).attr("href");
+    const top = $(id).offset().top;
+    $("html, body").animate({scrollTop: top}, 1000);
 });
